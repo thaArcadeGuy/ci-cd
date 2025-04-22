834f4a348d30b38852cf15b74aa8a58cdbae910f
thaArcadeGuy <gilbertsilver64@gmail.com>
2025-04-22 19:09:25 +0300

    Added Jenkinfile

---
+++ Jenkinsfile
@@ -0,0 +1,22 @@
+pipeline {
+  agent {
+    docker {
+      image "python: 3.9"
+    }
+  }
+  stages {
+    stage ("Setup") {
+      steps {
+        sh "python -m venv venv"
+        sh ". venv/bin/activate"
+        sh "pip install -r requirements.txt"
+      }
+    }
+    stage ("Test") {
+      steps {
+        sh ". venv/bin/activate"
+        sh "pytest"
+      }
+    }
+  }
+}
--- greeting/tests.py
+++ greeting/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+class GreetingTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_page(self):
+        response = self.client.get(reverse("index"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello")
+        self.assertTemplateUsed(response, "index.html")
+
+
+class GreetingFunctionalityTests(TestCase):
+    def test_greeting(self):
+        response = self.client.get(reverse("index"))
+
+        self.assertEqual(response.context["greeting"], "Hello")
+        # self.assertContains(response, "Hello")
+        # self.assertTemplateUsed(response, "index.html")
